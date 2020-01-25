9ef03f4a5571832c6ddf01d9e7e6cee3853bc6ca
Carlos Munoz <42192364+codewithcarlos@users.noreply.github.com>
2020-01-25 17:41:50 -0600

    Render page with react

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/../client/dist"));
+
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
